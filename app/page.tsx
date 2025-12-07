2cca6a12f8076d1bc65bdf7481aafca11ee2289f
WorldSportStars <tvkonflikt@gmail.com>
2025-12-07 20:29:41 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import { Wallet } from "@coinbase/onchainkit/wallet";
+import { Swap } from "@coinbase/onchainkit/swap";
+import type { Token } from "@coinbase/onchainkit/token";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
+// import { useQuickAuth } from "@coinbase/onchainkit/minikit";
+import styles from "./page.module.css";
+
+const eth: Token = {
+  name: "Ethereum",
+  address: "",
+  symbol: "ETH",
+  decimals: 18,
+  image:
+    "https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png",
+  chainId: 8453,
+};
+
+const usdc: Token = {
+  name: "USD Coin",
+  address: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+  symbol: "USDC",
+  decimals: 6,
+  image:
+    "https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2",
+  chainId: 8453,
+};
+
+export default function Home() {
+  // If you need to verify the user's identity, you can use the useQuickAuth hook.
+  // This hook will verify the user's signature and return the user's FID. You can update
+  // this to meet your needs. See the /app/api/auth/route.ts file for more details.
+  // Note: If you don't need to verify the user's identity, you can get their FID and other user data
+  // via `useMiniKit().context?.user`.
+  // const { data, isLoading, error } = useQuickAuth<{
+  //   userFid: string;
+  // }>("/api/auth");
+
+  const { setMiniAppReady, isMiniAppReady } = useMiniKit();
+
+  useEffect(() => {
+    if (!isMiniAppReady) {
+      setMiniAppReady();
+    }
+  }, [setMiniAppReady, isMiniAppReady]);
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.headerWrapper}>
+        <Wallet />
+      </header>
+
+      <div className={styles.content}>
+        <Image
+          priority
+          src="/sphere.svg"
+          alt="Sphere"
+          width={200}
+          height={200}
+        />
+        <h1 className={styles.title}>MiniKit</h1>
+
+        <p>
+          Get started by editing <code>app/page.tsx</code>
+        </p>
+
+        <div style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <Swap from={[eth]} to={[usdc]} />
+        </div>
+
+        <h2 className={styles.componentsTitle}>Explore Components</h2>
+
+        <ul className={styles.components}>
+          {[
+            {
+              name: "Transaction",
+              url: "https://docs.base.org/onchainkit/transaction/transaction",
+            },
+            {
+              name: "Swap",
+              url: "https://docs.base.org/onchainkit/swap/swap",
+            },
+            {
+              name: "Checkout",
+              url: "https://docs.base.org/onchainkit/checkout/checkout",
+            },
+            {
+              name: "Wallet",
+              url: "https://docs.base.org/onchainkit/wallet/wallet",
+            },
+            {
+              name: "Identity",
+              url: "https://docs.base.org/onchainkit/identity/identity",
+            },
+          ].map((component) => (
+            <li key={component.name}>
+              <a target="_blank" rel="noreferrer" href={component.url}>
+                {component.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
